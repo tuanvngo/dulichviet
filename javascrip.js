b0f61e08a4f31c7e53d35973bc681d980564d152
tuanvngo <88735944+tuanvngo@users.noreply.github.com>
2021-10-04 20:42:11 +0700

    sua file

--- javascrip.js
+++ javascrip.js
@@ -1,10 +1,8 @@
-const menuBars = document.querySelector('.header__top i')
-function open() {
-    document.querySelector('.header__top__list').classList.toggle('active')
-}
-menuBars.addEventListener('click',open )
-
-const search =document.querySelector('.header__top div span i')
-search.addEventListener('onclick', function(){
-    document.querySelector('.header__top div span input').classList.toggle('open_in')
+const toP=document.querySelector(".header");
+window.addEventListener("scroll", function(){
+    const x = window.pageYOffset;
+    if(x>100){
+        toP.classList.add("active")
+    }
+    else{toP.classList.remove("active")}
 })
